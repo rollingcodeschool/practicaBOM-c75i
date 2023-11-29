62aeb02625dd7946f9ea93cce7c384aa30f0f757
Emilse Arias <emilsearias@gmail.com>
2023-11-29 18:02:11 -0300

    agregamos un ejemplo de setTimeout

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//setTimeout
+function saludar(){
+    document.write('<p>Hola mundo 🗺️</p>');
+}
+
+window.setTimeout( saludar, 5000);
+
+setTimeout(()=>{
+    document.write('<p>Hola mundo nuevo 🗺️</p>');
+}, 3000);
+
+//setInterval
